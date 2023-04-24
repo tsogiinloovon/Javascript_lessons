1f9d27006a6b4faa5f3f20e29980cd358c4ec124
tsogiinloovon tsogtbaatar <tobi@tsogiinloovons-MacBook-Pro-2.local>
2023-04-24 22:30:12 +0800

    second lessons

--- app.js
+++ app.js
@@ -25,11 +25,36 @@ var person = {
   name: "Tsogiinlovon Tsogtbaatar",
   job: "Software engineer",
   age: "25",
-//   is_married: true,
-//   wife_name: "Khasherdene Baasandorj",
+  // is_married: true,
+  // wife_name: "Khasherdene Baasandorj",
   getName: function () {
     return this.name;
   },
 }; //.getName() //IIFE;
 
-console.log("person.name::: ", person.getName());
+// console.log("person.name::: ", person.getName());
+
+// Утган хандалт болох заагч хандалт
+
+//Value(Primitive) - Утган хандалт
+// var a = 12;
+// var b = a;
+// b = 13;
+// console.log("a=", a); // 12
+// console.log("b=", b); // 13
+
+//Reference (Oject,massive,Function) заалт
+// Javascript has 3 data types that are passed by reference: Array , Function , and Object .
+var p1 = {
+  name: "notebook",
+  price: "300$",
+};
+
+var p2 = p1;
+console.log("p1::: ", p1); //  { name: 'notebook', price: '300$' }
+console.log("p2::: ", p2); // { name: 'notebook', price: '300$' }
+
+p2.price = "400$"; // both object's price changed
+
+console.log("p1::: ", p1); //  { name: 'notebook', price: '400$' }
+console.log("p2::: ", p2); // { name: 'notebook', price: '400$' }
